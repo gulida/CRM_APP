a24cc43d2f7391edbf7bb3ce37143b7dca682b71
Kimsanova <synak2013@gmail.com>
2020-08-20 00:42:23 +0600

    Exchage rates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,12 +41,29 @@ export default {
   methods: {
     async refresh () {
       this.loading = true
-      this.currency = await this.$store.dispatch('fetchCurrency')
+      const currencies = await this.$store.dispatch('fetchCurrency')
+      this.currency = {
+        rates: {
+          EUR: 1,
+          USD: currencies.rates.USD,
+          RUB: currencies.rates.RUB
+        },
+        date: currencies.date
+      }
       this.loading = false
     }
   },
   async mounted() {
-    this.currency = await this.$store.dispatch('fetchCurrency')
+    const currencies = await this.$store.dispatch('fetchCurrency')
+    this.currency = {
+      rates: {
+        EUR: 1,
+        USD: currencies.rates.USD,
+        RUB: currencies.rates.RUB
+      },
+      date: currencies.date
+    }
+    console.log(this.currency)
     this.loading = false
   },
   components: {
